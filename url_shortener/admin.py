a6131f672be34b25c72ba40a9bc41e3eb5692f8f
Albert Suraliński <albert.suralinski@gmail.com>
2020-07-13 23:05:38 +0200

    Added basic admin view with urls and hashes.

--- url_shortener/admin.py
+++ url_shortener/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from url_shortener.models import Url
+
+
+class UrlAdmin(admin.ModelAdmin):
+    model = Url
+    list_display = ['user_url', 'url_hash']
+
+
+admin.site.register(Url, UrlAdmin)
