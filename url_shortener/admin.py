e0b2efc06b69e39379f63ddddbd6b3fe267c0203
Albert Suraliński <albert.suralinski@gmail.com>
2020-07-14 23:50:40 +0200

    Fixed spaces to PEP8 standard.

--- url_shortener/admin.py
+++ url_shortener/admin.py
@@ -7,8 +7,8 @@ class UrlStoreAdmin(admin.ModelAdmin):
     model = UrlStore
     list_display = ['user_url', 'url_hash']
     list_per_page = 25
-    search_fields=('user_url',)
-    list_filter=('user_url',)
+    search_fields = ('user_url',)
+    list_filter = ('user_url',)
 
 
 admin.site.register(UrlStore, UrlStoreAdmin)
