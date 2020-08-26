a9699b29346e664413dd86ee13cf7336fad4c134
Albert Suraliński <albert.suralinski@gmail.com>
2020-08-26 10:49:36 +0200

    Refactored view with used formview base class.

--- url_shortener/views.py
+++ url_shortener/views.py
@@ -1,33 +1,33 @@
 import logging
 
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
 from django.views import View
-
+from django.views.generic import FormView
+from django.urls import reverse
 from url_shortener.forms import UrlStoreForm
 from url_shortener.models import UrlStore
 
 logger = logging.getLogger(__name__)
 
 
-class UrlView(View):
+class UrlView(FormView):
 
-    def get(self, request, *args, **kwargs):
-        form = UrlStoreForm()
-        context = {'form': form}
-        return render(request, 'url_form.html', context)
+    template_name = 'url_form.html'
+    form_class = UrlStoreForm
 
-    def post(self, request, *args, **kwargs):
-        form = UrlStoreForm(data=request.POST)
-        ctx = {'form': form}
-        if form.is_valid():
-            url, is_created = UrlStore.objects.get_or_create(user_url=form.cleaned_data['user_url'])
-            short_url = url.get_short_url()
-            if is_created:
-                logger.debug("Created %s" % url)
-                logger.debug('Successfully hashed provided URL with value: %s' % short_url)
-            ctx.update({'short_url': short_url, 'url': url, 'is_created': is_created})
-        return render(request, 'url_form.html', ctx)
+    def form_valid(self, form):
+        url, is_created = UrlStore.objects.get_or_create(user_url=form.cleaned_data['user_url'])
+        short_url = url.get_short_url()
+        if is_created:
+            logger.debug("Created %s" % url)
+            logger.debug('Successfully hashed provided URL with value: %s' % short_url)
+        return self.render_to_response(
+            context=self.get_context_data(short_url=short_url, url=url, is_created=is_created)
+        )
+
+    def get_success_url(self):
+        return reverse('home')
 
 
 class UrlRedirectView(View):
