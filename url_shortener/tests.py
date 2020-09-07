5d3cba67e42a3349920acf6b53e2c4f9294089d1
Albert Suraliński <albert.suralinski@gmail.com>
2020-09-07 07:59:02 +0200

    Added tests of main form view.

--- url_shortener/tests.py
+++ url_shortener/tests.py
@@ -1,6 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 
+from url_shortener.models import UrlStore
 from url_shortener.utils import encode_string
 
 
@@ -57,5 +58,34 @@ class TestStringEncoder(TestCase):
 
 class TestUrlView(TestCase):
 
-    def test__aa(self):
-        self.client.post(reverse('home'), )
+    def setUp(self) -> None:
+        """This method runs before the execution of each test case."""
+        self.client = Client()
+        self.url = reverse("home")
+
+    def test__does_url_show_url_form(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.template_name[0], 'url_form.html')
+
+    def test__is_provided_valid_url_in_post_response(self):
+        data = {'user_url': "https://www.google.pl"}
+        response = self.client.post(self.url, data)
+        self.assertContains(response, data['user_url'])
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.template_name[0], 'url_form.html')
+
+    def test__response_with_provided_invalid_url(self):
+        data = {'user_url': "www.google.pl"}
+        response = self.client.post(self.url, data)
+        self.assertContains(response, "Enter a valid URL.")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.template_name[0], 'url_form.html')
+
+    def test__is_provided_duplicated_valid_url_in_post_response(self):
+        data = {'user_url': "https://www.google.pl"}
+        self.client.post(self.url, data)
+        second_response = self.client.post(self.url, data)
+        self.assertContains(second_response, data['user_url'])
+        self.assertContains(second_response, "Your provided URL already exist!")
+        self.assertEqual(second_response.status_code, 200)
+        self.assertEqual(second_response.template_name[0], 'url_form.html')
